f223fc1b04cdcf5ad24cfa05bbcccf0af240da21
ashesash <aishwarya.s@live.com>
2023-07-21 16:49:29 +1000

    contact form added

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Image from 'next/image';
 import ContactImg from '../public/assets/contact.png';
+import Link from 'next/link';
 import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from 'react-icons/ai'
 import { FaLinkedinIn, Fa500Px, FaGithub } from 'react-icons/fa'
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
 
 const Contact = () => {
     return (
@@ -40,9 +42,50 @@ const Contact = () => {
                             </div>
                         </div>
                     </div>
-                    <div>
-                        
+                    <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+                        <div className='p-4'>
+                            <form>
+                                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                                    <div className='flex flex-col'>
+                                        <label className='uppercase text-sm py-2'>Name</label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-200' type='text' />
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <label className='uppercase text-sm py-2'>Number</label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-200' type='text' />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className='flex flex-col py-2'>
+                                        <label className='uppercase text-sm py-2'>Email</label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-200' type='email' />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className='flex flex-col py-2'>
+                                        <label className='uppercase text-sm py-2'>Subject</label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-200' type='text' />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className='flex flex-col py-2'>
+                                        <label className='uppercase text-sm py-2'>Message</label>
+                                        <textarea className='border-2 rounded-lg p-3 flex border-gray-200' rows='5'></textarea>
+                                    </div>
+                                </div>
+                                <div>
+                                    <button className='w-full p-4 text-gray-200 mt-4'>Submit</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
+                </div>
+                <div className='flex justify-center py-12'>
+                    <Link href='/'>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover-scale-110 ease-in duration-300'>
+                            <HiOutlineChevronDoubleUp size={25} className='m-auto text-[#4d797b]'/>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
