5d10215c6c5dd3627e4973650643cc1e4bb7d49c
ashesash <aishwarya.s@live.com>
2023-07-21 16:49:55 +1000

    navbar shadow adjusted

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,31 @@
-import React,{useState} from 'react'
+import React, { useState,useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import {AiOutlineClose, AiOutlineMenu, AiOutlineMail} from 'react-icons/ai'
-import {FaLinkedinIn,Fa500Px,FaGithub} from 'react-icons/fa'
+import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from 'react-icons/ai'
+import { FaLinkedinIn, Fa500Px, FaGithub } from 'react-icons/fa'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
+    const [shadow, setShadow] = useState(false)
 
     const handleNav = () => {
-        setNav(!nav)
-    }
+        setNav(!nav);
+    };
+
+    useEffect(() => {
+        const handleShadow = () => {
+            if (window.scrollY >=90) {
+                setShadow(true)
+            }
+            else {
+                setShadow(false)
+            }
+        }
+        window.addEventListener('scroll', handleShadow)
+    }, [])
 
     return (
-        <div className='fixed w-full h-20 shadow-xl z-[100] bg-[#ecf0f3] px-4'>
+        <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100] bg-[#ecf0f3] px-4':'fixed w-full h-20 z-[100] bg-[#ecf0f3] px-4'}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Image src="/../public/assets/as.png" alt="/" width='130' height='50'></Image>
                 <div>
@@ -34,20 +47,20 @@ const Navbar = () => {
                         </Link>
                     </ul>
                     <div onClick={handleNav} className='md:hidden'>
-                        <AiOutlineMenu size={25}/>
+                        <AiOutlineMenu size={25} />
                     </div>
                 </div>
             </div>
-            <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70': ''}>
-                <div className={nav 
-                                ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md::w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
-                                : 'fixed left-[-100%] top-0 w-[75%] sm:w-[60%] md::w-[45%] h-screen p-10 ease-in duration-500'
-                                }>
+            <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+                <div className={nav
+                    ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md::w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                    : 'fixed left-[-100%] top-0 w-[75%] sm:w-[60%] md::w-[45%] h-screen p-10 ease-in duration-500'
+                }>
                     <div>
                         <div className='flex w-full items-center justify-between'>
-                            <div className='flex items-start'><Image src="/../public/assets/as.png" width='40' height='20' alt='/'/></div>
+                            <div className='flex items-start'><Image src="/../public/assets/as.png" width='40' height='20' alt='/' /></div>
                             <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer'>
-                                <AiOutlineClose size={15}/>
+                                <AiOutlineClose size={15} />
                             </div>
                         </div>
                         {/* <div className='border-b border-gray-300 my-4'>
