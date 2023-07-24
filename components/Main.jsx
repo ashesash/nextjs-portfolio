b4527e38762cf480c26c5898f2ebdc1ff7f397b5
ashesash <aishwarya.s@live.com>
2023-07-24 13:19:12 +1000

    spelling

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from 'react-icons/ai'
 import { FaLinkedinIn, Fa500Px, FaGithub } from 'react-icons/fa'
+import Link from 'next/link'
 
 const Main = () => {
     return (
@@ -12,21 +13,40 @@ const Main = () => {
                     <h1 className='py-2 text-gray-700'>A Software Engineer</h1>
                     <p className='text-sm tracking-widest text-gray-500 py-4 max-w-[70%] m-auto'>I&#39;m a software engineer with keen interest in full stack development and data engineering.</p>
                     <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover-scale-105 ease-in duration-500'>
-                            <FaLinkedinIn />
-                        </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover-scale-105 ease-in duration-500'>
-                            <FaGithub />
-                        </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover-scale-105 ease-in duration-500'>
-                            <Fa500Px />
-                        </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover-scale-105 ease-in duration-500'>
-                            <AiOutlineMail />
-                        </div>
+                        <a
+                            href='https://www.linkedin.com/in/aishwaryasahu/'
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                <FaLinkedinIn />
+                            </div>
+                        </a>
+                        <a
+                            href='https://github.com/ashesash'
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                <FaGithub />
+                            </div>
+                        </a>
+                        <Link href='/#contact'>
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                <AiOutlineMail />
+                            </div>
+                        </Link>
+                        <a
+                            href='https://500px.com/p/aishwaryasahu'
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                <Fa500Px />
+                            </div>
+                        </a>
                     </div>
                 </div>
-
             </div>
         </div>
     );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
                             </Link>
                         </ul>
                         <div className='pt-40'>
-                            <p className='uppercase tracking-widest text-[#4d797b]'> Let&#39s Connect</p>
+                            <p className='uppercase tracking-widest text-[#4d797b]'> Let&#39;s Connect</p>
                             <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
                                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover-scale-105 ease-in duration-500'>
                                     <FaLinkedinIn />
