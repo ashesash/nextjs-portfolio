dc3724ec77f0a7c6cce6df1c53b9a15b906f3dd7
ashesash <ashsahu@outlook.com>
2024-12-12 16:18:23 +1100

    landing page revamped

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,57 +1,84 @@
-import React from 'react'
-import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from 'react-icons/ai'
-import { FaLinkedinIn, Fa500Px, FaGithub } from 'react-icons/fa'
-import {ImProfile} from 'react-icons/im'
-import Link from 'next/link'
-// import Resume from '../public/assets/Tech'
+import React, { useState, useEffect } from 'react'
+import { motion, AnimatePresence, useAnimation } from 'framer-motion';
 
 const Main = () => {
+
+    const roles = [
+        { name: "+ Mechanical Engineer" },
+        { name: "+ Tinkerer" },
+        { name: "+ Artist" },
+        // { name: "" },
+    ]
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isInitialDelayComplete, setIsInitialDelayComplete] = useState(false);
+
+    useEffect(() => {
+        const initialDelayTimer = setTimeout(() => {
+            setIsInitialDelayComplete(true);
+        }, 2500);
+
+        let intervalId;
+        if (isInitialDelayComplete) {
+            intervalId = setInterval(() => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % roles.length);
+            }, 2000);
+        }
+
+        return () => {
+            clearTimeout(initialDelayTimer);
+            if (intervalId) 
+                clearInterval(intervalId);
+        };
+    }, [roles.length, isInitialDelayComplete]);
+
     return (
-        <div id='home' className='pt-15 w-full h-screen text-center'>
-            <div className='max-w-7xl w-full h-screen mx-auto p-2 flex justify-center items-center'>
-                <div class='fade-up'>
-                    <p className='uppercase text-sm tracking-widest text-gray-500 py-2 dark:text-[#ecf0f3]'>Let&#39;s build great products</p>
-                    <h1 className='py-4 text-gray-700]'>Hi, I&#39;m <span className='text-[#4d797b]'>Ash!</span></h1>
-                    <h1 className='py-2 text-gray-700]'>A Software Engineer</h1>
-                    <p className='text-sm tracking-widest text-gray-500 py-4 max-w-[70%] m-auto dark:text-[#ecf0f3]'>I&#39;m a software engineer with keen interest in full stack development and data engineering.</p>
-                    <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-                        <a
-                            href='https://www.linkedin.com/in/aishwaryasahu/'
-                            target='_blank'
-                            rel='noreferrer'
-                        >
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <FaLinkedinIn />
-                            </div>
-                        </a>
-                        <a
-                            href='https://github.com/ashesash'
-                            target='_blank'
-                            rel='noreferrer'
-                        >
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <FaGithub />
-                            </div>
-                        </a>
-                        <Link href='mailto:ashsahu@outlook.com'>
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <AiOutlineMail />
-                            </div>
-                        </Link>
-                        <Link href='/assets/TechCV_ASahu.pdf ' download='asahuResume.pdf'>
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <ImProfile />
-                            </div>
-                        </Link>
-                        <a
-                            href='https://500px.com/p/aishwaryasahu'
-                            target='_blank'
-                            rel='noreferrer'
-                        >
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <Fa500Px />
-                            </div>
-                        </a>
+        <div id='home' className='w-full h-screen text-start'>
+            <div className='w-full h-screen mx-[2%] flex items-center'>
+                <div>
+                    <motion.div
+                        initial={{ opacity: 0, x: -30 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{
+                            duration: 1,
+                            ease: "easeInOut"
+                        }}
+                    >
+                        <h1 className='py-4 text-gray-600 text-9xl'>
+                            Hi, I&#39;m <span className='bg-gradient-to-r from-teal-200 via-cyan-700 to-teal-200 bg-clip-text text-transparent bg-[length:200%] [animation:_gradient-move_3s_infinite_linear]'>Ash!</span>
+                        </h1>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{
+                            duration: 1,
+                            ease: "easeInOut",
+                            delay: 1
+                        }}
+                    >
+                        <h1 className='py-2 text-gray-600 leading-tight'>
+                            A passionate <br /> Software Engineer
+                        </h1>
+                    </motion.div>
+                    <div className='w-full h-16 overflow-hidden relative'>
+                        <AnimatePresence mode="popLayout">
+                            {isInitialDelayComplete && (
+                                <motion.h1
+                                    key={currentIndex}
+                                    initial={{ y: 50, opacity: 1 }}
+                                    animate={{ y: -1, opacity: 1 }}
+                                    exit={{ y: -51, opacity: 0 }}
+                                    transition={{
+                                        duration: 1,
+                                        // ease: "linear"
+                                    }}
+                                    className='text-gray-600 h-16 w-[vw]'
+                                >
+                                    {roles[currentIndex].name}
+                                </motion.h1>
+                            )}
+                        </AnimatePresence>
                     </div>
                 </div>
             </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-import '@/styles/globals.css'
-import { ThemeProvider } from 'next-themes'
-// import Layout from "../components/Layout";
+import '@/styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 export default function App({ Component, pageProps }) {
-  return <ThemeProvider attribute="class">
-    <Component {...pageProps} />
-  </ThemeProvider>
-
+  return (
+    <ThemeProvider attribute="class">
+      <ParallaxProvider>
+        <Component {...pageProps} />
+      </ParallaxProvider>
+    </ThemeProvider>
+  );
 }
